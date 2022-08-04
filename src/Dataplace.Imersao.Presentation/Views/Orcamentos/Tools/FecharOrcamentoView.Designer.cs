ba56dd6084ce398e3b06c4b585288f90e7fcaf2d
Bruno Alves <bruno.alves5009@gmail.com>
2022-08-03 23:13:02 -0300

    Desafio finalizado

---
+++ src/Dataplace.Imersao.Presentation/Views/Orcamentos/Tools/FecharOrcamentoView.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Dataplace.Imersao.Presentation.Views.Orcamentos.Tools
+{
+    partial class FecharOrcamentoView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FecharOrcamentoView));
+            this.gridPanel = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.tsiMarcar = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsiDesmarca = new System.Windows.Forms.ToolStripMenuItem();
+            this.rangeDate = new dpLibrary05.ucSymGen_ReferenceDtp();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkAberto = new System.Windows.Forms.CheckBox();
+            this.chkCancelado = new System.Windows.Forms.CheckBox();
+            this.chkFechado = new System.Windows.Forms.CheckBox();
+            this.gpbPeriodo = new System.Windows.Forms.GroupBox();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.gpbPeriodo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gridPanel
+            // 
+            this.gridPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridPanel.Location = new System.Drawing.Point(3, 94);
+            this.gridPanel.Name = "gridPanel";
+            this.gridPanel.Size = new System.Drawing.Size(1004, 455);
+            this.gridPanel.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(2, 552);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(100, 25);
+            this.toolStrip1.TabIndex = 501;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsiMarcar,
+            this.tsiDesmarca});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(88, 22);
+            this.toolStripSplitButton1.Text = "Ferramentas";
+            // 
+            // tsiMarcar
+            // 
+            this.tsiMarcar.Name = "tsiMarcar";
+            this.tsiMarcar.Size = new System.Drawing.Size(164, 22);
+            this.tsiMarcar.Text = "Marcar Todos";
+            this.tsiMarcar.Click += new System.EventHandler(this.tsiMarcar_Click);
+            // 
+            // tsiDesmarca
+            // 
+            this.tsiDesmarca.Name = "tsiDesmarca";
+            this.tsiDesmarca.Size = new System.Drawing.Size(164, 22);
+            this.tsiDesmarca.Text = "Desmarcar Todos";
+            this.tsiDesmarca.Click += new System.EventHandler(this.tsiDesmarca_Click);
+            // 
+            // rangeDate
+            // 
+            this.rangeDate.Date1CustomFormat = dpLibrary05.ucSymGen_ReferenceDtp.CustomFormatEnum.FShort;
+            this.rangeDate.Date2CustomFormat = dpLibrary05.ucSymGen_ReferenceDtp.CustomFormatEnum.FShort;
+            this.rangeDate.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.rangeDate.DotNetContainer = false;
+            this.rangeDate.KeyPreview = false;
+            this.rangeDate.Label1Text = dpLibrary05.ucSymGen_ReferenceDtp.LabelTextEnum.TFrom;
+            this.rangeDate.Label2Text = dpLibrary05.ucSymGen_ReferenceDtp.LabelTextEnum.TTo;
+            this.rangeDate.Location = new System.Drawing.Point(7, 20);
+            this.rangeDate.Margin = new System.Windows.Forms.Padding(4);
+            this.rangeDate.Name = "rangeDate";
+            this.rangeDate.OpenModal = false;
+            this.rangeDate.Parameters = ((System.Collections.Generic.IDictionary<string, object>)(resources.GetObject("rangeDate.Parameters")));
+            this.rangeDate.Size = new System.Drawing.Size(272, 22);
+            this.rangeDate.TabIndex = 503;
+            this.rangeDate.TabOrderScheme = dpLibrary05.TabOrderManager.TabScheme.None;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkAberto);
+            this.groupBox1.Controls.Add(this.chkCancelado);
+            this.groupBox1.Controls.Add(this.chkFechado);
+            this.groupBox1.Location = new System.Drawing.Point(3, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(110, 87);
+            this.groupBox1.TabIndex = 502;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Situação";
+            // 
+            // chkAberto
+            // 
+            this.chkAberto.AutoSize = true;
+            this.chkAberto.Checked = true;
+            this.chkAberto.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAberto.Location = new System.Drawing.Point(11, 17);
+            this.chkAberto.Margin = new System.Windows.Forms.Padding(2);
+            this.chkAberto.Name = "chkAberto";
+            this.chkAberto.Size = new System.Drawing.Size(57, 17);
+            this.chkAberto.TabIndex = 0;
+            this.chkAberto.Text = "Aberto";
+            this.chkAberto.UseVisualStyleBackColor = true;
+            // 
+            // chkCancelado
+            // 
+            this.chkCancelado.AutoSize = true;
+            this.chkCancelado.Location = new System.Drawing.Point(11, 59);
+            this.chkCancelado.Margin = new System.Windows.Forms.Padding(2);
+            this.chkCancelado.Name = "chkCancelado";
+            this.chkCancelado.Size = new System.Drawing.Size(77, 17);
+            this.chkCancelado.TabIndex = 2;
+            this.chkCancelado.Text = "Cancelado";
+            this.chkCancelado.UseVisualStyleBackColor = true;
+            // 
+            // chkFechado
+            // 
+            this.chkFechado.AutoSize = true;
+            this.chkFechado.Location = new System.Drawing.Point(11, 38);
+            this.chkFechado.Margin = new System.Windows.Forms.Padding(2);
+            this.chkFechado.Name = "chkFechado";
+            this.chkFechado.Size = new System.Drawing.Size(68, 17);
+            this.chkFechado.TabIndex = 1;
+            this.chkFechado.Text = "Fechado";
+            this.chkFechado.UseVisualStyleBackColor = true;
+            // 
+            // gpbPeriodo
+            // 
+            this.gpbPeriodo.Controls.Add(this.rangeDate);
+            this.gpbPeriodo.Location = new System.Drawing.Point(118, 3);
+            this.gpbPeriodo.Name = "gpbPeriodo";
+            this.gpbPeriodo.Size = new System.Drawing.Size(303, 86);
+            this.gpbPeriodo.TabIndex = 504;
+            this.gpbPeriodo.TabStop = false;
+            this.gpbPeriodo.Text = "Período";
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCarregar.Location = new System.Drawing.Point(925, 65);
+            this.btnCarregar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(82, 24);
+            this.btnCarregar.TabIndex = 505;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            this.btnCarregar.Click += new System.EventHandler(this.btnCarregar_Click);
+            // 
+            // FecharOrcamentoView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.gpbPeriodo);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.gridPanel);
+            this.Name = "FecharOrcamentoView";
+            this.Size = new System.Drawing.Size(1010, 609);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FecharOrcamentoView_KeyDown);
+            this.Controls.SetChildIndex(this.gridPanel, 0);
+            this.Controls.SetChildIndex(this.toolStrip1, 0);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.gpbPeriodo, 0);
+            this.Controls.SetChildIndex(this.btnCarregar, 0);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.gpbPeriodo.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gridPanel;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem tsiMarcar;
+        private System.Windows.Forms.ToolStripMenuItem tsiDesmarca;
+        internal dpLibrary05.ucSymGen_ReferenceDtp rangeDate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkAberto;
+        private System.Windows.Forms.CheckBox chkCancelado;
+        private System.Windows.Forms.CheckBox chkFechado;
+        private System.Windows.Forms.GroupBox gpbPeriodo;
+        private System.Windows.Forms.Button btnCarregar;
+    }
+}
---
+++ src/Dataplace.Imersao.Presentation/Views/Orcamentos/Tools/FecharOrcamentoView.cs
@@ -0,0 +1,253 @@
+﻿using C1.Win.C1TrueDBGrid;
+using Dataplace.Core.Application.Services.Results;
+using Dataplace.Core.Comunications;
+using Dataplace.Core.Domain.Localization.Messages.Extensions;
+using Dataplace.Core.Domain.Notifications;
+using Dataplace.Core.win.Controls.List.Behaviors;
+using Dataplace.Core.win.Controls.List.Behaviors.Contracts;
+using Dataplace.Core.win.Controls.List.Configurations;
+using Dataplace.Imersao.Core.Application.Orcamentos.Commands;
+using Dataplace.Imersao.Core.Application.Orcamentos.Queries;
+using Dataplace.Imersao.Core.Application.Orcamentos.ViewModels;
+using Dataplace.Imersao.Core.Domain.Orcamentos.Enums;
+using dpLibrary05.Infrastructure.Helpers.Permission;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dataplace.Imersao.Presentation.Views.Orcamentos.Tools
+{
+    public partial class FecharOrcamentoView : dpLibrary05.Infrastructure.UserControls.ucSymGen_ToolDialog
+    {
+        #region Fields
+        private const int _itemSeg = 467;
+
+        private C1TrueDBGrid _grid = new C1TrueDBGrid();
+        private IListBehavior<OrcamentoViewModel, OrcamentoQuery> _orcamentoList;
+        #endregion
+
+        #region Constructor
+        public FecharOrcamentoView()
+        {
+            InitializeComponent();
+            InitializeGrid();
+
+            _orcamentoList = new C1TrueDBGridListBehavior<OrcamentoViewModel, OrcamentoQuery>(_grid)
+            .WithConfiguration(GetConfiguration());
+
+            this.ToolConfiguration += FecharOrcamentoView_ToolConfiguration; ;
+            this.BeforeProcess += FecharOrcamentoView_BeforeProcess;
+            this.Process += FecharOrcamentoView_Process;
+        }
+        #endregion
+
+        #region Methods
+        //fiz somente dessa forma pq meu computador pessoal está sem o component one, então não dá pra arrastar o grid
+        private void InitializeGrid()
+        {
+            gridPanel.Controls.Add(_grid);
+            _grid.Dock = DockStyle.Fill;
+        }
+        #endregion
+
+        #region Tool Events
+        private void FecharOrcamentoView_ToolConfiguration(object sender, ToolConfigurationEventArgs e)
+        {
+            this.Text = "Fechar orçamentos em aberto";
+            e.SecurityIdList.Add(_itemSeg);
+            e.CancelButtonVisisble = true;
+        }
+
+        private void FecharOrcamentoView_BeforeProcess(object sender, BeforeProcessEventArgs e)
+        {
+            var permission = PermissionControl.Factory().ValidatePermission(_itemSeg, dpLibrary05.mGenerico.PermissionEnum.Execute);
+            if (!permission.IsAuthorized())
+            {
+                e.Cancel = true;
+                this.Message.Info(permission.BuildMessage());
+                return;
+            }
+
+            var itensSelecionados = _orcamentoList.GetCheckedItems();
+            if (itensSelecionados.Count() == 0)
+            {
+                e.Cancel = true;
+                this.Message.Info(53727.ToMessage());
+                return;
+            }
+
+            e.Parameter.Items.Add("itensSelecionados", itensSelecionados);
+        }
+
+        private async void FecharOrcamentoView_Process(object sender, ProcessEventArgs e)
+        {
+            if (!(e.Parameter.Items.get_Item("itensSelecionados").Value is IEnumerable<OrcamentoViewModel> itensSelecionados))
+            {
+                e.Cancel = true;
+                return;
+            }
+
+            e.ProgressMinimum = 0;
+            e.ProgressMaximum = itensSelecionados.Count();
+            e.BeginProcess();
+            // um a um
+            foreach (var item in itensSelecionados)
+            {
+                using (var scope = dpLibrary05.Infrastructure.ServiceLocator.ServiceLocatorScoped.Factory())
+                {
+                    var command = new FecharOrcamentoCommand(item);
+
+                    var mediator = scope.Container.GetInstance<IMediatorHandler>();
+                    var notifications = scope.Container.GetInstance<INotificationHandler<DomainNotification>>();
+
+                    await mediator.SendCommand(command);
+
+                    item.Result = Result.ResultFactory.New(notifications.GetNotifications());
+                    if (item.Result.Success)
+                    {
+                        item.IsSelected = false;
+                        item.Situacao = OrcamentoStatusEnum.Fechado.ToDataValue();
+                        e.LogBuilder.Items.Add($"Orçamento {item.NumOrcamento} fechado!", dpLibrary05.Infrastructure.Helpers.LogBuilder.LogTypeEnum.Information);
+                    }
+                    else
+                    {
+                        foreach (var notification in item.Result.Notifications)
+                            e.LogBuilder.Items.Add($"Orçamento {item.NumOrcamento} - {notification.Message.Trim()}", dpLibrary05.Infrastructure.Helpers.LogBuilder.LogTypeEnum.Err);
+                    }
+                }
+
+                if (e.CancellationRequested)
+                    break;
+
+                e.ProgressValue += 1;
+            }
+
+            e.EndProcess();
+        }
+        #endregion
+
+        #region List Events
+        private ViewModelListBuilder<OrcamentoViewModel> GetConfiguration()
+        {
+            var builder = new ViewModelListBuilder<OrcamentoViewModel>();
+            builder.AllowFilter();
+            builder.AllowSort();
+            builder.HasHighlight(x =>
+            {
+                x.Add(orcamento => orcamento.Situacao == OrcamentoStatusEnum.Cancelado.ToDataValue(), Color.Red);
+            });
+
+            builder.WithQuery<OrcamentoQuery>(() => GetData());
+
+            builder.Ignore(x => x.CdEmpresa);
+            builder.Ignore(x => x.CdFilial);
+            builder.Ignore(x => x.SqTabela);
+            builder.Ignore(x => x.CdTabela);
+            builder.Ignore(x => x.DtFechamento);
+            builder.Ignore(x => x.CdVendedor);
+            builder.Ignore(x => x.DataValidade);
+            builder.Ignore(x => x.DiasValidade);
+
+
+            builder.Property(x => x.NumOrcamento)
+                   .HasMinWidth(100)
+                   .HasCaption("#");
+
+            builder.Property(x => x.CdCliente)
+                   .HasMinWidth(100)
+                   .HasCaption("Cliente");
+
+            builder.Property(x => x.DsCliente)
+                   .HasMinWidth(300)
+                   .HasCaption("Razão");
+
+            builder.Property(x => x.DtOrcamento)
+                   .HasMinWidth(100)
+                   .HasCaption("Data")
+                   .HasFormat("d");
+
+            builder.Property(x => x.ValotTotal)
+                   .HasMinWidth(100)
+                   .HasCaption("Total")
+                   .HasFormat("n");
+
+            builder.Property(x => x.Situacao)
+                   .HasMinWidth(100)
+                   .HasCaption("Situação")
+                   .HasValueItems(x =>
+                   {
+                       x.Add(OrcamentoStatusEnum.Aberto.ToDataValue(), 3469.ToMessage());
+                       x.Add(OrcamentoStatusEnum.Fechado.ToDataValue(), 3846.ToMessage());
+                       x.Add(OrcamentoStatusEnum.Cancelado.ToDataValue(), 3485.ToMessage());
+                   });
+
+            return builder;
+        }
+
+        private OrcamentoQuery GetData()
+        {
+            var situacaoList = new List<OrcamentoStatusEnum>();
+            if (chkAberto.Checked)
+                situacaoList.Add(OrcamentoStatusEnum.Aberto);
+            if (chkFechado.Checked)
+                situacaoList.Add(OrcamentoStatusEnum.Fechado);
+            if (chkCancelado.Checked)
+                situacaoList.Add(OrcamentoStatusEnum.Cancelado);
+
+            DateTime? dtInicio = null;
+            DateTime? dtFim = null;
+            if (rangeDate.Date1.Value is DateTime d)
+                dtInicio = d;
+
+            if (rangeDate.Date2.Value is DateTime d2)
+                dtFim = d2;
+
+            var query = new OrcamentoQuery() { SituacaoList = situacaoList, DtInicio = dtInicio, DtFim = dtFim };
+
+            return query;
+        }
+        #endregion
+
+        #region Control Events
+        private void tsiMarcar_Click(object sender, EventArgs e)
+        {
+            _orcamentoList.ChangeCheckState(true);
+        }
+
+        private void tsiDesmarca_Click(object sender, EventArgs e)
+        {
+            _orcamentoList.ChangeCheckState(false);
+        }
+
+        private async void FecharOrcamentoView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+            {
+                await _orcamentoList.LoadAsync();
+            }
+
+            if (e.Control && e.Shift && e.KeyCode == Keys.M)
+            {
+                _orcamentoList.ChangeCheckState(true);
+            }
+
+            if (e.Control && e.Shift && e.KeyCode == Keys.D)
+            {
+                _orcamentoList.ChangeCheckState(false);
+            }
+        }
+
+        private async void btnCarregar_Click(object sender, EventArgs e)
+        {
+            await _orcamentoList.LoadAsync();
+        }
+        #endregion
+    }
+}
